0ff212db6b8bdde4a246ed036436e941a498ceb1
Pla9er <drugiekonto_99@o2.pl>
2024-02-23 02:15:23 +0100

    highlight diffrent colors

--- app/src/components/project/files/File.svelte
+++ app/src/components/project/files/File.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-	import Highlight, { LineNumbers } from "svelte-highlight";
-	import { windows10 } from "svelte-highlight/styles";
-    import { java } from "svelte-highlight/languages";
+    import langHighlight from "$lib/langHighlight";
+    import Highlight, { LineNumbers } from "svelte-highlight";
+    import { windows10 } from "svelte-highlight/styles";
 
-	export let code: string;
+    export let code: string;
+    export let lang: string;
 </script>
 
 <svelte:head>
-	{@html windows10}
+    {@html windows10}
 </svelte:head>
 
 <div class="code">
-	<Highlight
-		language={java}
-		{code}
-		let:highlighted
-		--border-color="var(--lightBorder)"
-		>
-		<LineNumbers {highlighted}/>
-	</Highlight>
+    <Highlight
+        language={langHighlight(lang)}
+        {code}
+        let:highlighted
+        --border-color="var(--lightBorder)"
+    >
+        <LineNumbers {highlighted} />
+    </Highlight>
 </div>
 
 <style lang="scss">
-	.code {
-		margin: 0 auto;
-		width: 100%;
-		height: max-content;
-		background-color: var(--background);
-		border: solid 1px var(--lightBorder);
-		border-radius: 5px;
-	}
+    .code {
+        margin: 0 auto;
+        width: 100%;
+        height: max-content;
+        background-color: var(--background);
+        border: solid 1px var(--lightBorder);
+        border-radius: 5px;
+    }
 </style>
--- app/src/routes/project/[id]/file/+page.svelte
+++ app/src/routes/project/[id]/file/+page.svelte
@@ -1,88 +1,96 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
-	import { page } from "$app/stores";
-	import DirectoryView from "$components/project/files/DirectoryView.svelte";
-	import File from "$components/project/files/File.svelte";
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+    import DirectoryView from "$components/project/files/DirectoryView.svelte";
+    import File from "$components/project/files/File.svelte";
 
-	export let data;
-	let isFile: boolean = data.data.folders === undefined ? true : false;
-	let path = $page.url.searchParams.get("path");
-	if (path === null) {
-		goto("/404");
-	}
+    export let data;
+    let isFile: boolean = data.data.folders === undefined ? true : false;
+    let path = $page.url.searchParams.get("path");
+    if (path === null) {
+        goto("/404");
+    }
 
-	const len = path ? path.length : 0;
-	const getFoldersStructure = () => {
-		let current = "";
-		const folderStructure: string[] = [];
+    const len = path ? path.length : 0;
+    const getFoldersStructure = () => {
+        let current = "";
+        const folderStructure: string[] = [];
 
-		for (let i = 0; i < len; i++) {
-			if (path![i] === "/" && current !== "") {
-				folderStructure.push(current);
-				current = "";
-			} else {
-				current += path![i];
-			}
-		}
-		folderStructure.push(current);
+        for (let i = 0; i < len; i++) {
+            if (path![i] === "/" && current !== "") {
+                folderStructure.push(current);
+                current = "";
+            } else {
+                current += path![i];
+            }
+        }
+        folderStructure.push(current);
 
-		return folderStructure;
-	};
-	const folderStructure = getFoldersStructure();
+        return folderStructure;
+    };
+    const folderStructure = getFoldersStructure();
+
+    function getFileExtension(filename: string | null): string {
+        if (!filename) return "";
+        const dotIndex = filename.lastIndexOf(".");
+        return dotIndex >= 0
+            ? filename.substring(dotIndex + 1).toLowerCase()
+            : "";
+    }
 </script>
 
 <main class="column">
-	<div class="row">
-		<a href=".">..</a>
-		{#each folderStructure as folder, i (i)}
-			<img src="/icons/arrow_right.svg" alt="" />
-			<a
-				href="?path={folderStructure.slice(0, i + 1).join('/')}"
-				data-sveltekit-reload>{folder.replaceAll("/", "")}</a
-			>
-		{/each}
-	</div>
-	{#if isFile}
-		<File code={data.data} />
-	{:else}
-		<DirectoryView data={data.data} slug={data.slug} />
-	{/if}
+    <div class="row">
+        <a href=".">..</a>
+        {#each folderStructure as folder, i (i)}
+            <img src="/icons/arrow_right.svg" alt="" />
+            <a
+                href="?path={folderStructure.slice(0, i + 1).join('/')}"
+                data-sveltekit-reload>{folder.replaceAll("/", "")}</a
+            >
+        {/each}
+    </div>
+    {#if isFile}
+        <File code={data.data} lang={getFileExtension(path)} />
+    {:else}
+        <DirectoryView data={data.data} slug={data.slug} />
+    {/if}
 </main>
 
 <style lang="scss">
-	main {
-		width: 90%;
-		max-width: 900px;
-		margin: 60px auto;
+    main {
+        width: 90%;
+        max-width: 900px;
+        margin: 60px auto;
 
-		.row {
-			height: 30px;
-			margin: 15px auto 15px 15px;
+        .row {
+            height: 30px;
+            margin: 15px auto 15px 15px;
 
-			img {
-				width: 20px;
-				margin: 0 3px;
-			}
-	
-			a {
-				width: 100%;
-				font-family: "Fira sans";
-				font-size: 15px;
-				color: #dfdcdc;
-				transition: text-decoration 150ms;
+            img {
+                width: 20px;
+                margin: 0 3px;
+            }
 
-				&:first-child {
-					margin: 0px;
-				}
-	
-				&:hover {
-					text-decoration: underline;
-				}
-	
-				&:last-of-type {
-					color: rgb(0, 81, 255);
-				}
-			}
-		}
-	}
+            a {
+                width: 100%;
+                font-family: "Fira sans";
+                font-size: 15px;
+                color: #dfdcdc;
+                transition: text-decoration 150ms;
+
+                &:first-child {
+                    margin: 0px;
+                }
+
+                &:hover {
+                    text-decoration: underline;
+                }
+
+                &:last-of-type {
+                    color: rgb(0, 81, 255);
+                }
+            }
+        }
+    }
 </style>
