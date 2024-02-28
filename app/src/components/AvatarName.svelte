35313ea0048cb4f7093457f974236984b90f3ef0
Pla9er <drugiekonto_99@o2.pl>
2024-02-28 17:47:28 +0100

    add chat btn on user profil page

--- app/src/components/AvatarName.svelte
+++ app/src/components/AvatarName.svelte
@@ -1,146 +1,174 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
+    import { goto } from "$app/navigation";
 
-	export let userdata: UserDto;
-	export let avatarSize = "100px";
+    export let userdata: UserDto;
+    export let avatarSize = "100px";
 
-	import fetchHttp from "$lib/fetchHttp";
-	import { get } from "svelte/store";
-	import Avatar from "./Avatar.svelte";
-	import FollowBtn from "./user/FollowBtn.svelte";
-	import { tokenStore } from "$lib/stores/tokenStore";
-	import { onMount } from "svelte";
+    import fetchHttp from "$lib/fetchHttp";
+    import { get } from "svelte/store";
+    import Avatar from "./Avatar.svelte";
+    import FollowBtn from "./user/FollowBtn.svelte";
+    import { tokenStore } from "$lib/stores/tokenStore";
+    import { onMount } from "svelte";
     import type { UserDto } from "$lib/models/user/UserDto";
 
-	const token = get(tokenStore);
+    const token = get(tokenStore);
 
-	var name = "";
-	if (userdata.firstname !== null && userdata.lastname !== null) {
-		name = userdata.firstname + " " + userdata.lastname;
-	}
+    var name = "";
+    if (userdata.firstname !== null && userdata.lastname !== null) {
+        name = userdata.firstname + " " + userdata.lastname;
+    }
 
-	async function follow() {
-		if (token === undefined) {
-			await goto("login");
-			return;
-		}
-		await fetchHttp(`/user/${userdata.username}/follow`, {
-			method: "post",
-			token: token,
-		});
+    async function follow() {
+        if (token === undefined) {
+            await goto("login");
+            return;
+        }
+        await fetchHttp(`/user/${userdata.username}/follow`, {
+            method: "post",
+            token: token,
+        });
 
-		userdata.followed = true;
-		userdata.followers += 1;
-	}
+        userdata.followed = true;
+        userdata.followers += 1;
+    }
 
-	async function unfollow() {
-		if (token === undefined) {
-			await goto("login");
-			return;
-		}
-		await fetchHttp(`/user/${userdata.username}/unfollow`, {
-			method: "put",
-			token: token,
-		});
+    async function unfollow() {
+        if (token === undefined) {
+            await goto("login");
+            return;
+        }
+        await fetchHttp(`/user/${userdata.username}/unfollow`, {
+            method: "put",
+            token: token,
+        });
 
-		userdata.followed = false;
-		userdata.followers -= 1;
-	}
+        userdata.followed = false;
+        userdata.followers -= 1;
+    }
 
-	let followBtnWidth = "120px";
+    let followBtnWidth = "120px";
 
-	onMount(() => {
-		if (window.innerWidth <= 476) {
-			followBtnWidth = "250px";
-			avatarSize = "80px";
-		}
+    onMount(() => {
+        if (window.innerWidth <= 476) {
+            followBtnWidth = "250px";
+            avatarSize = "80px";
+        }
 
-		onresize = (e) => {
-			if (window.innerWidth <= 476) {
-				followBtnWidth = "250px";
-				avatarSize = "80px";
-			} else {
-				followBtnWidth = "120px";
-				avatarSize = "100px";
-			}
-		};
-	});
+        onresize = () => {
+            if (window.innerWidth <= 476) {
+                followBtnWidth = "250px";
+                avatarSize = "80px";
+            } else {
+                followBtnWidth = "120px";
+                avatarSize = "100px";
+            }
+        };
+    });
 </script>
 
 <div class="avatarName">
-	<Avatar size={avatarSize} username={userdata.username} />
-	<div class="textData">
-		<h1>{userdata.username}</h1>
-		<p>{name}</p>
-		<div class="follows">
-			<p>{userdata.followers} followers {userdata.following} following</p>
-		</div>
-	</div>
-	{#if token !== undefined}
-		{#if userdata.username != JSON.parse(atob(token.split(".")[1])).sub}
-			<div id="followBtn_" style="margin-left: auto;">
-				<FollowBtn
-					width={followBtnWidth}
-					following={userdata.followed}
-					onFollow={follow}
-					onUnfollow={unfollow}
-				/>
-			</div>
-		{/if}
-	{/if}
+    <Avatar size={avatarSize} username={userdata.username} />
+    <div class="textData">
+        <h1>{userdata.username}</h1>
+        <p>{name}</p>
+        <div class="follows">
+            <p>{userdata.followers} followers {userdata.following} following</p>
+        </div>
+    </div>
+    {#if token !== undefined}
+        {#if userdata.username != JSON.parse(atob(token.split(".")[1])).sub}
+            <div id="actionBtns" class="row" style="margin-left: auto;">
+                <FollowBtn
+                    width={followBtnWidth}
+                    following={userdata.followed}
+                    onFollow={follow}
+                    onUnfollow={unfollow}
+                />
+                <a
+                    href={location.href.replace(
+                        location.host,
+                        location.host + "/chat"
+                    )}
+                    class="msgBtn row"
+                >
+                    Chat
+                </a>
+            </div>
+        {/if}
+    {/if}
 </div>
 
 <style lang="scss">
-	.avatarName {
-		width: 100%;
-		max-width: 900px;
-		display: flex;
-		flex-wrap: wrap;
-		align-items: center;
+    .avatarName {
+        width: 100%;
+        max-width: 900px;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
 
-		.textData {
-			margin-left: 30px;
-			margin-right: 40px;
+        .textData {
+            margin-left: 30px;
+            margin-right: 40px;
 
-			h1 {
-				margin: 0;
-				color: #fff;
-				font-family: "Fira sans";
-			}
+            h1 {
+                margin: 0;
+                color: #fff;
+                font-family: "Fira sans";
+            }
 
-			p {
-				margin: 0;
-				margin-left: 1px;
-				font-family: "Fira sans";
-				color: rgb(185, 185, 185);
-			}
+            p {
+                margin: 0;
+                margin-left: 1px;
+                font-family: "Fira sans";
+                color: rgb(185, 185, 185);
+            }
 
-			.follows {
-				display: flex;
-				align-items: center;
-				justify-content: space-around;
+            .follows {
+                display: flex;
+                align-items: center;
+                justify-content: space-around;
 
-				p {
-					margin-top: 4px;
-					margin-left: -3px;
-					color: var(--mainColor);
-					font-size: 10px;
-				}
-			}
-		}
-	}
+                p {
+                    margin-top: 4px;
+                    margin-left: -3px;
+                    color: var(--mainColor);
+                    font-size: 10px;
+                }
+            }
+        }
 
-	@media screen and (max-width: 476px) {
-		.avatarName {
-			justify-content: center;
-			margin-top: -15px;
-		}
+        .msgBtn {
+            width: 72px;
+            height: 30px;
+            margin-left: 8px;
+            background-color: #f1f1f1;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            justify-content: center;
+            font-weight: 600;
+            font-family: sans-serif;
+            color: #000;
+        }
+    }
 
-		#followBtn_ {
-			width: 100%;
-			display: flex;
-			margin: 10px 0;
-			justify-content: center;
-		}
-	}
+    @media screen and (max-width: 512px) {
+        .avatarName {
+            justify-content: center;
+            margin-top: -15px;
+        }
+
+        #actionBtns {
+            width: 90%;
+            max-width: 360px;
+            display: flex;
+            margin: 10px auto;
+            justify-content: center;
+        }
+
+        .msgBtn {
+            min-width: 50%;
+        }
+    }
 </style>
--- app/src/routes/[username]/+page.svelte
+++ app/src/routes/[username]/+page.svelte
@@ -1,77 +1,79 @@
 <script lang="ts">
-	export let data;
+    export let data;
 
-	import AvatarName from '$components/AvatarName.svelte';
-	import SwitchButton from '$components/project/SwitchButton.svelte';
-	import Readme from '$components/project/files/Readme.svelte';
-	import UserProjects from '$components/user/UserProjects.svelte';
+    import AvatarName from "$components/AvatarName.svelte";
+    import SwitchButton from "$components/project/SwitchButton.svelte";
+    import Readme from "$components/project/files/Readme.svelte";
+    import UserProjects from "$components/user/UserProjects.svelte";
 
-	let switchValue = 'Description';
+    let switchValue = "Description";
 
-	let projectSwitchBtnText = `Projects (${data.data.projects.length})`;
+    let projectSwitchBtnText = `Projects (${data.data.projects.length})`;
 </script>
 
 <svelte:head>
-	<title>{data.data.username}</title>
+    <title>{data.data.username}</title>
 </svelte:head>
 
 <main>
-	<AvatarName userdata={data.data} avatarSize="140px" />
-	<div class="links">
-		{#each data.data.personalLinks as link}
-			{#if link !== ''}
-				<div class="link">
-					<img src="/icons/link.svg" alt="link" />
-					<a href={link}>{link}</a>
-				</div>
-			{/if}
-		{/each}
-	</div>
-	<SwitchButton bind:currentValue={switchValue} values={['Description', projectSwitchBtnText]} />
-	{#if switchValue === projectSwitchBtnText}
-		<UserProjects projects={data.data.projects} />
-	{:else if switchValue === 'Description'}
-		{#if data.data.description && data.data.description != ""}
-			<Readme html={data.data.description} />
-		{/if}
-	{/if}
+    <AvatarName userdata={data.data} avatarSize="140px" />
+    <div class="links">
+        {#each data.data.personalLinks as link}
+            {#if link !== ""}
+                <div class="link">
+                    <img src="/icons/link.svg" alt="link" />
+                    <a href={link}>{link}</a>
+                </div>
+            {/if}
+        {/each}
+    </div>
+    <SwitchButton
+        bind:currentValue={switchValue}
+        values={["Description", projectSwitchBtnText]}
+    />
+    {#if switchValue === projectSwitchBtnText}
+        <UserProjects projects={data.data.projects} />
+    {:else if switchValue === "Description"}
+        {#if data.data.description && data.data.description != ""}
+            <Readme readme={data.data.description} />
+        {/if}
+    {/if}
 </main>
 
 <style lang="scss">
-	main {
-		width: 90%;
-		max-width: 1000px;
-		margin: 0 auto;
-		margin-top: 30px;
+    main {
+        width: 90%;
+        max-width: 1000px;
+        margin: 0 auto;
+        margin-top: 30px;
 
-		.links {
-			margin-top: 20px;
-			margin-left: 20px;
-			display: flex;
-			flex-direction: column;
-			
-			.link {
-				display: flex;
-				align-items: center;
-				margin: 2px 0;
+        .links {
+            margin-top: 20px;
+            margin-left: 20px;
+            display: flex;
+            flex-direction: column;
 
-				a {
-					max-width: 220px;
-					overflow-x: auto;
-					font-size: 15px;
-					font-family: sans-serif;
-					font-weight: 300;
-					color: rgb(0, 110, 255);
-					text-decoration: none;
-				}
-	
-				img {
-					width: 20px;
-					margin-top: 2px;
-					margin-right: 8px;
-				}
-			}
-		}
+            .link {
+                display: flex;
+                align-items: center;
+                margin: 2px 0;
 
-	}
+                a {
+                    max-width: 220px;
+                    overflow-x: auto;
+                    font-size: 15px;
+                    font-family: sans-serif;
+                    font-weight: 300;
+                    color: rgb(0, 110, 255);
+                    text-decoration: none;
+                }
+
+                img {
+                    width: 20px;
+                    margin-top: 2px;
+                    margin-right: 8px;
+                }
+            }
+        }
+    }
 </style>
