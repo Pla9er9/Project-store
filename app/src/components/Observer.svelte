545684822dd173b0912b7f8b178c7f98e18bc187
Pla9er <drugiekonto_99@o2.pl>
2024-03-03 15:32:23 +0100

    add observer component for smooth pagination

---
+++ app/src/components/Observer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    export let onvisible: () => void;
+    
+    onMount(() => {
+        let observer = new IntersectionObserver(onvisible, {});
+        const observerDiv = document.querySelector(".observer");
+        if (!observerDiv) {
+            console.error("Cannot initialize observer")
+            return
+        }
+        observer.observe(observerDiv);
+    });
+</script>
+
+<div class="observer"></div>
+
+<style>
+    .observer {
+        visibility: hidden;
+        width: 100%;
+        height: 20px;
+    }
+</style>
